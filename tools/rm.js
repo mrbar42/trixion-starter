a8634d0fac3cbe5dc280f4528d8e0ee3e119d08d
mrbar42 <mrbar42@gmail.com>
2016-05-29 11:42:05 +0300

    improved functionality

--- tools/rm.js
+++ tools/rm.js
@@ -1,22 +1,28 @@
 'use strict';
 
-var path = process.argv[2];
+var paths = process.argv.slice(2);
 
 const fs = require('fs');
 function deleteFolderRecursive(path) {
   if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach(function (file, index) {
-      var curPath = path + "/" + file;
-      if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-    fs.rmdirSync(path);
+    if (fs.statSync(path).isDirectory()) {
+      fs.readdirSync(path).forEach(function (file, index) {
+        var curPath = path + "/" + file;
+        if (fs.lstatSync(curPath).isDirectory()) { // recurse
+          deleteFolderRecursive(curPath);
+        } else { // delete file
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    }
+    else {
+      fs.unlinkSync(path);
+    }
   }
 }
 
-if (path) {
-  deleteFolderRecursive(path)
-}
+paths
+  .forEach(function (path) {
+    deleteFolderRecursive(path);
+  });
